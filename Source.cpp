a2e8b0b62255c975096510b3ee2e3f88d7a0be84
illya <korsillua@gmail.com>
2024-11-23 17:43:13 +0200

    completed task 2

--- Source.cpp
+++ Source.cpp
@@ -1,6 +1,25 @@
 #include <iostream>
+#include <Windows.h> 
 using namespace std;
 
 int main() {
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    const int size = 5; 
+    int arr[size];      
+
+    cout << "Введіть " << size << " елементів масиву:" << endl;
+    for (int i = 0; i < size; i++) {
+        cout << "Елемент " << i + 1 << ": ";
+        cin >> arr[i];
+    }
+
+    cout << "Масив у зворотному порядку:" << endl;
+    for (int i = size - 1; i >= 0; i--) {
+        cout << arr[i] << " ";
+    }
+
+    cout << endl;
 	return 0;
 }
